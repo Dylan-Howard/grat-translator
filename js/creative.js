87bc2ebd94d64833fb9fe64c951a325e2542c931
Dylan Howard <dylanhoward6@gmail.com>
2017-09-15 13:56:33 -0500

    Updated styling

--- js/creative.js
+++ js/creative.js
@@ -28,10 +28,6 @@
         }
     })
 
-    // $('#translation').change(function() {
-    //   $('#favorite').find('.bot').removeClass('active');
-    // })
-
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
@@ -48,21 +44,5 @@
         scale: 0.3,
         distance: '0px'
     }, 300);
-
-    // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    });
 
 })(jQuery); // End of use strict
